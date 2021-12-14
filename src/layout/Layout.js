a3f8691cf6722ea36c032596492386c930a21dae
Angela Louh <louh.angela@gmail.com>
2021-12-14 14:50:25 -0800

    creating frontend repo

---
+++ src/layout/Layout.js
@@ -0,0 +1,36 @@
+import React from "react";
+import Menu from "./Menu";
+import Routes from "./Routes";
+
+import "./Layout.css";
+
+/**
+ * Defines the main layout of the application.
+ *
+ * You will not need to make changes to this file.
+ *
+ * @returns {JSX.Element}
+ */
+function Layout() {
+  return (
+    <div className="container-fluid">
+      <div className="row h-100">
+        <div className="col-lg-2 side-bar">
+          <Menu />
+        </div>
+        <div className="col" style={{ padding: "0px" }}>
+          <img
+            src="/images/restaurant-lighting-1.jpg"
+            class="img-fluid"
+            alt="Dashboard banner"
+          ></img>
+          <div style={{ padding: "15px" }}>
+            <Routes />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Layout;
